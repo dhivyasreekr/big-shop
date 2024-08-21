261302e9aebba6025521d2669af597f8e4d93723
dhivyasreekr <dhivyasreekr@gmail.com>
2024-08-21 13:26:53 +0530

    top rated, and recently added partial setup

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -54,6 +54,12 @@
 
                     <!-- Include the trending_products slider partial -->
                     @include('frontend.slider.trending_products.type1')
+
+                    <!-- Include the top_rated slider partial -->
+                    @include('frontend.slider.top_rated.type1')
+
+                    <!-- Include the recently_added slider partial -->
+                    @include('frontend.slider.recently_added.type1')
                 </div>
             </div>
         </section>

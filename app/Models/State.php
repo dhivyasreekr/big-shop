33b4ed6a1623ed5ad436299e97c94cb21b41e381
dhivyasreekr <dhivyasreekr@gmail.com>
2024-08-24 18:44:30 +0530

    State model created

---
+++ app/Models/State.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class State extends Model
+{
+    use HasFactory;
+
+    protected $table = 'states';
+
+    protected $fillable=[
+
+        'country_id',
+        'name',
+        'code'
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class, 'country_id');
+    }
+}

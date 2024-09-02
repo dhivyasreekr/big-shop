d2a6dfda9d362d63a9d00c40814c69853675ec4a
dhivyasreekr <dhivyasreekr@gmail.com>
2024-09-02 18:45:34 +0530

    Update app.blade.php

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -14,9 +14,10 @@
     <meta property="og:url" content="" />
     <meta property="og:image" content="" />
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="frontend/imgs/theme/favicon.svg" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/imgs/theme/favicon.svg') }}" />
     <!-- Template CSS -->
-    <link rel="stylesheet" href="frontend/css/main.css?v=5.3" />
+    <link rel="stylesheet" href="{{ asset('frontend/css/plugins/animate.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('frontend/css/main.css?v=5.3') }}" />
 </head>
 <body>
 
@@ -42,6 +43,12 @@
         Request::routeIs('product.show')
         
     )    
+
+        @yield('content')
+
+        @elseif (
+            request()->query('category')
+        )    
 
         @yield('content')
 
@@ -99,28 +106,28 @@
     @include('frontend.preloader.type1')
 
     <!-- Vendor JS-->
-    <script src="frontend/js/vendor/modernizr-3.6.0.min.js"></script>
-    <script src="frontend/js/vendor/jquery-3.6.0.min.js"></script>
-    <script src="frontend/js/vendor/jquery-migrate-3.3.0.min.js"></script>
-    <script src="frontend/js/vendor/bootstrap.bundle.min.js"></script>
-    <script src="frontend/js/plugins/slick.js"></script>
-    <script src="frontend/js/plugins/jquery.syotimer.min.js"></script>
-    <script src="frontend/js/plugins/wow.js"></script>
-    <script src="frontend/js/plugins/perfect-scrollbar.js"></script>
-    <script src="frontend/js/plugins/magnific-popup.js"></script>
-    <script src="frontend/js/plugins/select2.min.js"></script>
-    <script src="frontend/js/plugins/waypoints.js"></script>
-    <script src="frontend/js/plugins/counterup.js"></script>
-    <script src="frontend/js/plugins/jquery.countdown.min.js"></script>
-    <script src="frontend/js/plugins/images-loaded.js"></script>
-    <script src="frontend/js/plugins/isotope.js"></script>
-    <script src="frontend/js/plugins/scrollup.js"></script>
-    <script src="frontend/js/plugins/jquery.vticker-min.js"></script>
-    <script src="frontend/js/plugins/jquery.theia.sticky.js"></script>
-    <script src="frontend/js/plugins/jquery.elevatezoom.js"></script>
+    <script src="{{ asset('frontend/js/vendor/modernizr-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/vendor/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/vendor/jquery-migrate-3.3.0.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/vendor/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/slick.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/jquery.syotimer.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/waypoints.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/wow.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/magnific-popup.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/select2.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/counterup.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/jquery.countdown.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/images-loaded.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/isotope.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/scrollup.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/jquery.vticker-min.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/jquery.theia.sticky.js') }}"></script>
+    <script src="{{ asset('frontend/js/plugins/jquery.elevatezoom.js') }}"></script>
     <!-- Template JS -->
-    <script src="./frontend/js/main.js?v=5.3"></script>
-    <script src="./frontend/js/shop.js?v=5.3"></script>
+    <script src="{{ asset('frontend/js/main.js?v=5.3') }}"></script>
+    <script src="{{ asset('frontend/js/shop.js?v=5.3') }}"></script>
 
 </body>
 </html>
